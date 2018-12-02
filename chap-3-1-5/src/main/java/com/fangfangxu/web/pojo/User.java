c3afb2b4191a423b2cf2884250e412efcdba5dc9
fangfangxu <472848022@qq.com>
2018-12-03 00:34:29 +0800

    工程目录修改与swagger代码提交

---
+++ chap-3-1-5/src/main/java/com/fangfangxu/web/pojo/User.java
@@ -0,0 +1,14 @@
+package com.fangfangxu.web.pojo;
+
+import lombok.Data;
+
+/**
+ * User实体类
+ */
+@Data
+public class User {
+    private Long id;
+    private String username;
+    private Integer age;
+
+}
