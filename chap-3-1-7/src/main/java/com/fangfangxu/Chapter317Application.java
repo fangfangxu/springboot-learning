be06749eb7f5c9acd5dfe5125f6ae10cdca83d01
xufangfang <xufangfang123>
2018-12-04 14:40:48 +0800

    317工程创建

---
+++ chap-3-1-7/src/main/java/com/fangfangxu/Chapter317Application.java
@@ -0,0 +1,12 @@
+package com.fangfangxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter317Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter317Application.class, args);
+    }
+}
