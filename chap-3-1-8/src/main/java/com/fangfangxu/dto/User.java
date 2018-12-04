e7b2a4cdd8afd39256222666391c5a65a05257de
xufangfang <xufangfang123>
2018-12-04 19:40:01 +0800

    318扩展xml请求和响应的支持

---
+++ chap-3-1-8/src/main/java/com/fangfangxu/dto/User.java
@@ -0,0 +1,15 @@
+package com.fangfangxu.dto;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+
+@Data
+@JacksonXmlRootElement(localName = "User")
+public class User {
+    @JacksonXmlProperty(localName = "name")
+    private String name;
+    @JacksonXmlProperty(localName = "age")
+    private Integer age;
+
+}
--- chap-3-1-8/src/main/java/com/fangfangxu/web/HelloController.java
+++ chap-3-1-8/src/main/java/com/fangfangxu/web/HelloController.java
@@ -1,7 +1,11 @@
 package com.fangfangxu.web;
 
+import com.fangfangxu.dto.User;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -10,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+    @RequestMapping(value = "/hello" ,
+            consumes = MediaType.APPLICATION_XML_VALUE,
+            produces = MediaType.APPLICATION_XML_VALUE,
+            method = RequestMethod.POST)
+    public User index(@RequestBody User user) {
+        user.setName("aaa");
+        return user;
         }
         }
