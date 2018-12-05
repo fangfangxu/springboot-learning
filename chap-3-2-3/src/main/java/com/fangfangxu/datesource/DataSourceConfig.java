b304915e3a86a51652514e366cc1b37110cd5bf2
fangfangxu <472848022@qq.com>
2018-12-05 21:02:43 +0800

    323基于Template的多数据源配置

---
+++ chap-3-2-3/src/main/java/com/fangfangxu/datesource/DataSourceConfig.java
@@ -0,0 +1,42 @@
+package com.fangfangxu.datesource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name="dataSource1")
+//    @Qualifier("dataSource1")
+    @ConfigurationProperties(prefix = "spring.datasource.primary")
+    public DataSource primaryDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name="dataSource2")
+//    @Qualifier("dataSource2")
+    @ConfigurationProperties(prefix = "spring.datasource.secondary")
+    public DataSource secondaryDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name="primaryJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(@Qualifier("dataSource1") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+
+    @Bean(name="secondaryJdbcTemplate")
+    public JdbcTemplate secondaryJdbcTemplate(@Qualifier("dataSource2") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+
+}
