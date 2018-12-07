3d8cafbfb2b4db179319efb11fc7c58806ed720a
fangfangxu <472848022@qq.com>
2018-12-07 17:14:15 +0800

    325集成redis代码提交

--- chap-3-2-5/src/test/java/com/fangfangxu/Chapter325ApplicationTests.java
+++ chap-3-2-5/src/test/java/com/fangfangxu/Chapter325ApplicationTests.java
@@ -1,18 +1,35 @@
 package com.fangfangxu;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Chapter325Application.class)
 public class Chapter325ApplicationTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
+    /**
+     * 通过编写测试用例，举例说明如何访问Redis
+     * @throws Exception
+     */
     @Test
     public void getHello() throws Exception {
-      
+        // 保存字符串
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
     }
 
 }
+
+
+
+
+
