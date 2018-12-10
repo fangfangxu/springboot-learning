4f1518f084be6845b14fbc574267267ef8da8756
fangfangxu <472848022@qq.com>
2018-12-10 14:43:48 +0800

    325springboot集成redis序列化对象代码提交

--- chap-3-2-5/src/main/java/com/fangfangxu/redis2/RedisConfig2.java
+++ chap-3-2-5/src/main/java/com/fangfangxu/redis2/RedisConfig2.java
@@ -2,6 +2,7 @@ package com.fangfangxu.redis2;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,13 +18,10 @@ public class RedisConfig2 {
     //
     @Autowired
     private RedisTemplate redisTemplate;
-//    @Value("${spring.redis.password}")
-//    @Order(1)
-    private String password="123456";
 
     @Bean(name="redis2")
-    public RedisTemplate redisTemplateInit() {
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+    public RedisTemplate redisTemplateInit(@Qualifier("jedis") JedisConnectionFactory jedis) {
+        redisTemplate.setConnectionFactory(jedis);
         //设置序列化Key的实例化对象
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         //设置序列化Value的实例化对象
@@ -31,19 +29,11 @@ public class RedisConfig2 {
         return redisTemplate;
     }
 
-    @Bean
-//    @Order(2)
-    JedisConnectionFactory jedisConnectionFactory() {
+    @Bean(name="jedis")
+    JedisConnectionFactory jedisConnectionFactory(@Value("${spring.redis.password}") String password) {
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
         jedisConnectionFactory.setPassword(password);
         return jedisConnectionFactory;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
