384a9ea730873742753432e624aefa16d829d20f
fangfangxu <472848022@qq.com>
2018-12-11 15:08:38 +0800

    327springboot集成Mybatis提交

--- chap-3-2-7/src/test/java/com/fangfangxu/Chapter327ApplicationTests.java
+++ chap-3-2-7/src/test/java/com/fangfangxu/Chapter327ApplicationTests.java
@@ -2,20 +2,29 @@ package com.fangfangxu;
 
 
 
+import com.fangfangxu.mapper.UserMapper;
+import com.fangfangxu.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Chapter327Application.class)
 public class Chapter327ApplicationTests {
-
+    @Autowired
+    private UserMapper userMapper;
     @Test
+    @Rollback
     public void getHello() throws Exception {
-
+        userMapper.insert("BBB", 20);
+        User u = userMapper.findByName("AAA");
+//        Assert.assertEquals(20, u.getAge().intValue());
     }
 
 }
