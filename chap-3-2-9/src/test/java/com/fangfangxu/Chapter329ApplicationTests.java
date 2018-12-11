7c05d5354f49d72a557b0b90ae3192e9b67880b7
fangfangxu <472848022@qq.com>
2018-12-11 20:03:30 +0800

    329工程创建

---
+++ chap-3-2-9/src/test/java/com/fangfangxu/Chapter329ApplicationTests.java
@@ -0,0 +1,20 @@
+package com.fangfangxu;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter329ApplicationTests {
+
+    @Test
+    public void getHello() throws Exception {
+
+    }
+
+}
