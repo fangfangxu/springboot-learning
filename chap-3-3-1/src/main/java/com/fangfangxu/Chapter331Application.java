8ed9af20e59ba26dfb06d9729604b1db4fcd9a82
fangfangxu <472848022@qq.com>
2018-12-12 15:28:05 +0800

    331工程创建

--- chap-3-3-1/src/main/java/com/fangfangxu/Chapter331Application.java
+++ chap-3-3-1/src/main/java/com/fangfangxu/Chapter331Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter3210Application {
+public class Chapter331Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Chapter3210Application.class, args);
+        SpringApplication.run(Chapter331Application.class, args);
     }
 }
--- chap-3-3-1/src/test/java/com/fangfangxu/Chapter331ApplicationTests.java
+++ chap-3-3-1/src/test/java/com/fangfangxu/Chapter331ApplicationTests.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 
-public class Chapter3210ApplicationTests {
+public class Chapter331ApplicationTests {
 
 
     @Test
