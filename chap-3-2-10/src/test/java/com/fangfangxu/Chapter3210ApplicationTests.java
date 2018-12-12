45a320bdcc61255e1d948f174a4284bd034a3ed5
fangfangxu <472848022@qq.com>
2018-12-12 14:55:57 +0800

    3210工程创建

---
+++ chap-3-2-10/src/test/java/com/fangfangxu/Chapter3210ApplicationTests.java
@@ -0,0 +1,23 @@
+package com.fangfangxu;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class Chapter3210ApplicationTests {
+
+
+    @Test
+    public void getHello() throws Exception {
+
+    }
+
+}
