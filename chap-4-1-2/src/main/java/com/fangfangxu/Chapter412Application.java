c77a45eec173c44f200f46b8c6df2794f7ded377
fangfangxu <472848022@qq.com>
2018-12-17 15:04:07 +0800

    412springboot使用@Async代码提交

--- chap-4-1-2/src/main/java/com/fangfangxu/Chapter412Application.java
+++ chap-4-1-2/src/main/java/com/fangfangxu/Chapter412Application.java
@@ -2,8 +2,10 @@ package com.fangfangxu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+//@EnableAsync
 public class Chapter412Application {
 
     public static void main(String[] args) {
--- chap-4-1-2/src/test/java/com/fangfangxu/Chapter412ApplicationTests.java
+++ chap-4-1-2/src/test/java/com/fangfangxu/Chapter412ApplicationTests.java
@@ -1,16 +1,22 @@
 package com.fangfangxu;
 
+import com.fangfangxu.task.Task;
 import com.fangfangxu.web.HelloController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.concurrent.Future;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,11 +24,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAsync
 public class Chapter412ApplicationTests {
-
+    @Autowired
+    private Task task;
 
     @Test
     public void getHello() throws Exception {
+        long start = System.currentTimeMillis();
+//        task.doTaskOne();
+//        task.doTaskTwo();
+//        task.doTaskThree();
+        Future<String> task1 = task.doTaskOne();
+        Future<String> task2 = task.doTaskTwo();
+        Future<String> task3 = task.doTaskThree();
+        while(true){
+            if(task1.isDone() && task2.isDone() && task3.isDone()){
+                break;
+            }
+            Thread.sleep(1000);//1s
+        }
+
+        long end=System.currentTimeMillis();
+        System.out.print("任务全部完成，总耗时：" + (end - start) + "毫秒");
 
     }
 
