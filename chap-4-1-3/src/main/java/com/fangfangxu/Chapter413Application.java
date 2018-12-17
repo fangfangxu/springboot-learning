4a1d7e4a0bd2f39d8e18bfb771da71302ec32c70
fangfangxu <472848022@qq.com>
2018-12-17 15:12:27 +0800

    413工程创建

---
+++ chap-4-1-3/src/main/java/com/fangfangxu/Chapter413Application.java
@@ -0,0 +1,12 @@
+package com.fangfangxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter413Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter413Application.class, args);
+    }
+}
