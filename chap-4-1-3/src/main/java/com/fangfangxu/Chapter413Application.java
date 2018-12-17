b235d61bc1752c348f5c651423f19968a92fb358
fangfangxu <472848022@qq.com>
2018-12-17 17:11:39 +0800

    Update Chapter413Application.java

--- chap-4-1-3/src/main/java/com/fangfangxu/Chapter413Application.java
+++ chap-4-1-3/src/main/java/com/fangfangxu/Chapter413Application.java
@@ -2,11 +2,34 @@ package com.fangfangxu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 public class Chapter413Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter413Application.class, args);
+    }
+    //自定义线程池
+    @EnableAsync
+    @Configuration
+    class TaskPoolConfig {
+        @Bean("taskExecutor")
+        public Executor taskExecutor() {
+            ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+            executor.setCorePoolSize(10);
+            executor.setMaxPoolSize(20);
+            executor.setQueueCapacity(200);
+            executor.setKeepAliveSeconds(60);
+            executor.setThreadNamePrefix("taskExecutor-");
+            executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+            return executor;
+        }
     }
 }
