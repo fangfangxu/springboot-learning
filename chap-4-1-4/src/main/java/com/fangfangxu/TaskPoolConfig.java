f0a11bd32343a949cb796b5053660bdd74cf6602
fangfangxu <472848022@qq.com>
2018-12-18 16:35:30 +0800

    414代码提交

---
+++ chap-4-1-4/src/main/java/com/fangfangxu/TaskPoolConfig.java
@@ -0,0 +1,25 @@
+package com.fangfangxu;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class TaskPoolConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskScheduler executor = new ThreadPoolTaskScheduler();
+        executor.setPoolSize(20);
+        executor.setThreadNamePrefix("taskExecutor-");
+
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        return executor;
+    }
+
+}
--- chap-4-1-4/src/test/java/com/fangfangxu/Chapter414ApplicationTests.java
+++ chap-4-1-4/src/test/java/com/fangfangxu/Chapter414ApplicationTests.java
@@ -1,30 +1,33 @@
 package com.fangfangxu;
 
-import com.fangfangxu.web.HelloController;
-import org.junit.Before;
+
+import lombok.SneakyThrows;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Chapter414Application.class)
 public class Chapter414ApplicationTests {
-
+    @Autowired
+    private Task task;
 
     @Test
+    @SneakyThrows
     public void getHello() throws Exception {
+        for (int i = 0; i < 1000000; i++) {
+            task.doTaskOne();
+            task.doTaskTwo();
+            task.doTaskThree();
 
+            if (i == 9999) {
+                System.exit(0);
+            }
+        }
     }
 
 }
