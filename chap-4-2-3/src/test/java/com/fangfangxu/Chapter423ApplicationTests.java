6e40ebfb99455c8b643946e5cb33b83ce2a719bf
fangfangxu <472848022@qq.com>
2018-12-23 14:21:34 +0800

    423springboot集成log4j日志框架的多环境配置

--- chap-4-2-3/src/test/java/com/fangfangxu/Chapter423ApplicationTests.java
+++ chap-4-2-3/src/test/java/com/fangfangxu/Chapter423ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.fangfangxu;
 
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -9,10 +10,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Chapter423Application.class)
 public class Chapter423ApplicationTests {
+    private Logger logger  = Logger.getLogger(getClass());
 
     @Test
     public void getHello() throws Exception {
-
+        logger.trace("Trace Message!");
+        logger.debug("Debug Message!");
+        logger.info("Info Message!");
+        logger.warn("Warn Message!");
+        logger.error("Error Message!");
+        logger.fatal("Fatal Message!");
     }
+
 
 }
