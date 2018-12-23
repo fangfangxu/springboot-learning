6ececd4780005d46d87db5a2c997c65669be24e2
fangfangxu <472848022@qq.com>
2018-12-23 13:50:15 +0800

    423工程创建

---
+++ chap-4-2-3/src/test/java/com/fangfangxu/Chapter423ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fangfangxu;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Chapter423Application.class)
+public class Chapter423ApplicationTests {
+
+    @Test
+    public void getHello() throws Exception {
+
+    }
+
+}
